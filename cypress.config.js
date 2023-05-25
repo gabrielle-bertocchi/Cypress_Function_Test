40c03babe9f871656706fa135d9f6407fdacbf11
bertocchi <gabrielle_bertocchi@hotmail.com>
2023-05-25 14:40:48 +0100

    .

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("cypress");
+module.exports = {
+  // ...
+  pageLoadTimeout: 90000,
+  // ...
+}
 
 module.exports = defineConfig({
   e2e: {
--- cypress/e2e/functional.cy.js
+++ cypress/e2e/functional.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Functional Test Swag Labs', () => {
+  it('Validate the Page Title ', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.title().should('eq', 'Swag Labs')
   })
+
+  it('Validate valid logging', () => {
+    cy.visit('https://www.saucedemo.com/')
+    get('.input_error[name="user-name"]').type('standard_user')
+    //continuar caso 2
+  });
 })
